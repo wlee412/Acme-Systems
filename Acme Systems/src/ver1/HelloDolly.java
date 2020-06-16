def25d4484653e0ccf5cae00840ff0b31f45ca28
William Lee <wjwl0412@gmail.com>
2020-06-16 23:30:39 +0900

    "from eclipse again" added

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -4,7 +4,8 @@ public class HelloDolly {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Dolly");
+		System.out.println("Hello Dolly from Eclipse");
+		System.out.println("Hello Dolly from Eclipse Again");
 
 
 	}
